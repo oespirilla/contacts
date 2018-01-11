8f67deaa8b699d3cff82f39ae0a0ee8c98458335
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-01-11 14:35:58 -0600

    added flex layout and contact manager module

---
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactmanager-app',
+  template: `
+    <p>
+      contactmanager-app works!
+    </p>
+  `,
+  styles: []
+})
+export class ContactmanagerAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/contactmanager/contactmanager.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactmanagerAppComponent } from './contactmanager-app.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContactmanagerAppComponent]
+})
+export class ContactmanagerModule { }
