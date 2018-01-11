306da919e407bac8dce5498d9e178d4c8bf56c3d
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-01-11 15:16:22 -0600

    added contact manager components

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: '', component: MainContentComponent}
+    ]},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ContactmanagerAppComponent]
+  declarations: [ContactmanagerAppComponent, ToolbarComponent, SidenavComponent, MainContentComponent]
 })
 export class ContactmanagerModule { }
