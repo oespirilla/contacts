99509fa17a06a451cf3c3d39f59e12d5c84667da
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-01-12 20:26:25 -0600

    added mat-table to notes in contact detail view

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Note } from '../../models/note';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
+  @Input() notes: Note[];
+
+  displayedColumns = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+
   constructor() { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
